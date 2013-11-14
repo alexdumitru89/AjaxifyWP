7db0ceecee62ad0889ff3ce6d066c897577b9a38
alexdumitru89 <me@alexdumitru.com>
2013-11-14 22:57:17 +0200

    Create AjaxifyWP.js

---
+++ AjaxifyWP.js
@@ -0,0 +1,27 @@
+/* 
+  Author: Alex Dumitru
+  Website: http://www.alexdumitru.eu
+  License: Free to use and distribute
+*/
+jQuery(document).ready(function($) {
+	loadPage = function(href,push) {
+		if (push) history.pushState('', '', href);
+			$main.load(href + " main>*", function(html) {
+			document.title = $(html).filter('title').text();
+		}
+	}
+	$(window).on("popstate", function(e) {
+		if (e.originalEvent.state !== null) {
+			loadPage(location.href,false);
+			e.preventDefault();
+		}
+	});
+	$(document).on("click", "a, area", function(e) {
+		var href = $(this).attr("href");
+		if (href.indexOf(document.domain) > -1 || href.indexOf(':') === -1) {
+			loadPage(href,true);
+			e.preventDefault();
+			return false;
+		}
+	});
+});
